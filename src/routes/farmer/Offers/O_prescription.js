815fb575482667f8428c8fb6cd5a797ce09654c9
Moheb <moheb2995@gmail.com>
2023-06-30 22:03:07 +0330

    V0.33fix

--- src/routes/farmer/Offers/O_prescription.js
+++ src/routes/farmer/Offers/O_prescription.js
@@ -6,6 +6,162 @@ const O_prescription = () => {
 return (
 <div>
   <h1 className='text'> هنوز نسخه ای ثبت نشده</h1>
+
+  <option value="PENDING ">در انتظار</option>
+  <option value="REJECTED">پذیرفته نشده</option>
+  <option value="DONE">انجام شده</option>
+
+
+  <option value="BROILER">گوشتی</option>
+  <option value="LAYING">تخم گذار</option>
+
+
+  <option value="DRINK">آشامیدنی</option>
+  <option value="EYE_DROP">قطره چشمی</option>
+  <option value="SPRAY">اسپری</option>
+  <option value="INTRAMUSCULAR_INJECTION">تزریق عضله</option>
+  <option value="SUBCUTANEOUS_INJECTION">تزریق زیر پوستی</option>
+  <option value="WING_WEB_PRICK">تلقیح در بال</option>
+
+
+  <option value="FISH_MEAL">پودر ماهی</option>
+  <option value="MEAT_MEAL ">پودر گوشت</option>
+  <option value="BLOOD_MEAL">پودر خون</option>
+  <option value="FEATHER_MEAL">پودر پر</option>
+  <option value="BONE_MEAL">پودر استخوان</option>
+  <option value="OYSTER_SHELL_FLOUR">پودر صدف</option>
+  <option value="LIMESTONE_POWDER">پودر آهک</option>
+  <option value="SOYBEAN_MEAL">کنجاله سویاv</option>
+  <option value="CANOLA_MEAL">کنجاله کانولا</option>
+  <option value="CORN_MEAL">کنجاله ذرت</option>
+  <option value="COTTON_SEED_MEAL">کنجاله تخم پنبه</option>
+  <option value="SUNFLOWER_MEAL">کنجاله آفتاب گردان</option>
+  <option value="PEANUT_MEAL">کنجاله بادام زمینی</option>
+  <option value="SESAME_MEAL">کنجاله کنجد</option>
+  <option value="CORN">ذرت</option>
+  <option value="WHEAT_BRAN">سبوس گندم</option>
+  <option value="WHEAT">گندم</option>
+  <option value="BARLEY">جو</option>
+  <option value="RICE_BRAN">سبوس برنج</option>
+  <option value="OATS">یولاف</option>
+  <option value="MILO">مایلو</option>
+  <option value="RYE">چاودار</option>
+  <option value="TRITICALE">تریتیکاله</option>
+  <option value="RICE">برنج</option>
+  <option value="MOLASSES">ملاس</option>
+  <option value="FLAX">کتان</option>
+  <option value="SORGHUM">سورگوم</option>
+  <option value="OIL">روغن</option>
+  <option value="COTTONSEED_OIL">روغن پنبه دانه</option>
+  <option value="CANOLA_OIL ">روغن کانولا</option>
+  <option value="SUNFLOWER_OIL"> روغن آفتاب گردان</option>
+  <option value="LINSEED_OIL">روغن بزرک</option>
+  <option value="PALM_OIL">روغن پالم</option>
+  <option value="SOYBEAN_OIL">روغن سویا</option>
+  <option value="FISH_OIL">روغن ماهی</option>
+  <option value="CHICKEN_FAT">چربی طیور</option>
+  <option value="VEGETABLE_OIL">روغن گیاهی</option>
+  <option value="SALT">نمک</option>
+  <option value="ENZYME">آنزیم</option>
+  <option value="PEA">نخود</option>
+  <option value="CALCIUM_CARBONATE">کلسیم کربنات</option>
+  <option value="DICALCIUM_PHOSPHATE">دی کلسیم فسفات</option>
+  <option value="MONOCALCIUM_PHOSPHATE"> مونو کلسیم فسفات</option>
+  <option value="SULFATE_CALCIUM">سولفات کلسیم</option>
+  <option value="MAGNESIUM_OXIDE">اکسید منیزیم</option>
+  <option value="PHOSPHATE">فسفات</option>
+  <option value="CHLORIDE_POTASSIUM">کلرید پتاسیم</option>
+  <option value="POTASSIUM_AND_MAGNESIUM_SULFATE">سولفات پتاسیم و منیزیم</option>
+  <option value="POTASSIUM_SULFATE">سولفات پتاسیم</option>
+  <option value="SODIUM_CARBONATE">کربنات سدیم</option>
+  <option value="SODIUM_DIPHOSPHATE">دی فسفات سدیم</option>
+  <option value="SODIUM_MONOPHOSPHATE">مونو فسفات سدیم</option>
+  <option value="SODIUM_SULFATE">سولفات سدیم</option>
+  <option value="PHOSPHORIC_ACID">اسیدفسفریک</option>
+  <option value="SELENIUM">سلنیوم</option>
+  <option value="METHIONINE">متیونین</option>
+  <option value="LYSINE"> لیزین</option>
+  <option value="ARGININE">آرژنین</option>
+  <option value="TRYPTOPHAN">تریپتوفان</option>
+  <option value="THREONINE">ترئونین</option>
+  <option value="GLYCINE">گلیسین</option>
+  <option value="GLUTAMIC_ACID"> اسیدگلوتامیک</option>
+  <option value="ISOLEUCINE">ایزولوسین</option>
+  <option value="VALINE">والین</option>
+  <option value="VITAMIN_D">ویتامین D</option>
+  <option value="VITAMIN_A">ویتامین A</option>
+  <option value="VITAMIN_K">ویتامین K</option>
+  <option value="VITAMIN_E ">ویتامین E</option>
+  <option value="VITAMIN_C"> ویتامین C</option>
+  <option value="VITAMIN_B1">ویتامین B1</option>
+  <option value="VITAMIN_B2 "> ویتامین B2</option>
+  <option value="VITAMIN_B6">ویتامین B6</option>
+  <option value="VITAMIN_B12">ویتامین B12</option>
+  <option value="B_COMPLEX">B کمپلکس</option>
+  <option value="MINERAL_SUPPORT">مکمل مواد معدنی</option>
+  <option value="VITAMIN_SUPPORT">مکمل ویتامین</option>
+  <option value="OYSTER_MOUNT">صدف کوهی</option>
+  <option value="OYSTER_SEA">صدف دریایی</option>
+
+
+  <option value="MAREK">مارک</option>
+  <option value="NEWCASTLE"> نیوکاسل</option>
+  <option value="NEWCASTLE_B1_STRAIN">B1</option>
+  <option value="NEWCASTLE_LASOTA_STRAIN">لاسوتا</option>
+  <option value="NEWCASTLE_CLONE30_STRAIN ">کلون30</option>
+  <option value="NEWCASTLE_CLONE_STRAIN">کلون</option>
+  <option value="NEWCASTLE_6_10_STRAIN">6/10</option>
+  <option value="NEWCASTLE_AVINEW_STRAIN">Avinew</option>
+  <option value="NEWCASTLE_VITAPEST_STRAIN">Vitapest</option>
+  <option value="GUMBORO">گامبرو (IBD)</option>
+  <option value="INFECTIOUS_BRONCHITIS">برونشیت (IBV)</option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+
+  
 </div>
 )}
 export default O_prescription
